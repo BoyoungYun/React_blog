3f4e1b157d1b30c31b0e1f4a5e34837821817707
Boyoung Yun <index_1234@naver.com>
2022-01-19 00:47:43 +0900

    modal component 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '여자 모자 추천']);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{글제목[0]} 
+        <span onClick={ ()=> {따봉변경(따봉 + 1)}}>👍</span>{따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div onClick={ () => modal변경(!modal)} className="list">
+        <h3>{글제목[2]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      {
+        modal === true
+        ? <Modal />
+        : null
+      }
     </div>
   );
 }
 
+function Modal(){
+  return(
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
+ /* 출처 : 코딩애플 - React 리액트 기초부터 쇼핑몰 프로젝트까지! */
