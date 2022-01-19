33d5dc5998e0532dddd40a267c84e7432536eac5
Boyoung Yun <index_1234@naver.com>
2022-01-19 23:06:00 +0900

    상세페이지 제작 (props 연습)

--- src/App.js
+++ src/App.js
@@ -5,43 +5,46 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '여자 모자 추천']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
-        <h3>{글제목[0]} 
-        <span onClick={ ()=> {따봉변경(따봉 + 1)}}>👍</span>{따봉}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div onClick={ () => modal변경(!modal)} className="list">
-        <h3>{글제목[2]}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      {
+        글제목.map(function(글,index)
+        {
+          return(
+            <div onClick={ () => modal변경(!modal)} className="list">
+              <h3 onClick={ () => 누른제목변경(index) }>{글}<span onClick={ ()=> {like(index)}}>👍</span>{따봉[index]}</h3>
+              <p>2월 17일 발행</p>
+              <hr/>
+            </div>
+          );
+        })
+      }
       {
         modal === true
-        ? <Modal />
+        ? <Modal 글제목={글제목} 누른제목={누른제목} />
         : null
       }
     </div>
   );
+  function like(index)
+  {
+    var newLike = 따봉;
+    newLike[index]++;
+    따봉변경(newLike);
+  }
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
